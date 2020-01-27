b74e65626a9454e567fdf54800bf1475eae65ace
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-01-28 00:26:33 +0530

    created flowerCatalogue.js file
    moved some app level functions to flowerCatalogue file

---
+++ flowerCatalogue.js
@@ -0,0 +1,73 @@
+const {readFileSync, existsSync, writeFileSync} = require('fs');
+const Response = require('./server/lib/response.js');
+const CONTENT_TYPE = require('./server/lib/mimeTypes.js');
+
+const previousComment = require(`${__dirname}/commentHistory.json`);
+
+const {
+  formatComment,
+  updateComments
+} = require('./public/js/commentsUpdater.js');
+
+const STATIC_FOLDER = `${__dirname}/public`;
+
+const successFulResponse = function(req, content) {
+  const [, extn] = req.url.match(/.*\.(.*)$/);
+  const contentType = CONTENT_TYPE[extn];
+  const res = new Response();
+  res.setHeader('content-type', contentType);
+  res.setHeader('content-length', content.length);
+  res.statusCode = 200;
+  res.msg = 'OK';
+  res.body = content;
+  return res;
+};
+
+const serveStaticFiles = function(req) {
+  if (!existsSync(req.url)) {
+    return new Response();
+  }
+  content = readFileSync(`${req.url}`);
+  return successFulResponse(req, content);
+};
+
+const servePost = function(req) {
+  const url = `${__dirname}/commentHistory.json`;
+  const formattedComment = formatComment(req.body);
+  previousComment.unshift(formattedComment);
+  writeFileSync(url, JSON.stringify(previousComment, null, 2));
+  return serveGuestBook(req);
+};
+
+const serveGuestBook = function(req) {
+  const content = updateComments(previousComment);
+  return successFulResponse(req, content);
+};
+
+const findHandler = function(req) {
+  if (req.url === '/') {
+    req.url = '/index.html';
+  }
+
+  if (req.method === 'GET' && req.url === '/guestBook.html') {
+    return serveGuestBook;
+  }
+
+  req.url = `${STATIC_FOLDER}${req.url}`;
+  if (req.method === 'POST') {
+    return servePost;
+  }
+
+  if (req.method === 'GET') {
+    return serveStaticFiles;
+  }
+
+  return () => new Response();
+};
+
+const processRequest = req => {
+  const handler = findHandler(req);
+  return handler(req);
+};
+
+module.exports = {processRequest};
--- server/server.js
+++ server/server.js
@@ -1,70 +1,6 @@
 const {Server} = require('net');
-const {readFileSync, existsSync, writeFileSync} = require('fs');
+const {processRequest} = require('../flowerCatalogue.js');
 const Request = require('./lib/request.js');
-const Response = require('./lib/response.js');
-const CONTENT_TYPE = require('./lib/mimeTypes.js');
-const previousComment = require(`${__dirname}/../commentHistory.json`);
-const {
-  formatComment,
-  updateComments
-} = require('../public/js/commentsUpdater.js');
-
-const STATIC_FOLDER = `${__dirname}/../public`;
-
-const successFulResponse = function(req, content) {
-  const [, extn] = req.url.match(/.*\.(.*)$/);
-  const contentType = CONTENT_TYPE[extn];
-  const res = new Response();
-  res.setHeader('content-type', contentType);
-  res.setHeader('content-length', content.length);
-  res.statusCode = 200;
-  res.msg = 'OK';
-  res.body = content;
-  return res;
-};
-
-const serveStaticFiles = function(req) {
-  if (!existsSync(req.url)) {
-    return new Response();
-  }
-  content = readFileSync(`${req.url}`);
-  return successFulResponse(req, content);
-};
-
-const servePost = function(req) {
-  const url = `${__dirname}/../commentHistory.json`;
-  const formattedComment = formatComment(req.body);
-  previousComment.unshift(formattedComment);
-  writeFileSync(url, JSON.stringify(previousComment, null, 2));
-  return serveGuestBook(req);
-};
-
-const serveGuestBook = function(req) {
-  const content = updateComments(previousComment);
-  return successFulResponse(req, content);
-};
-
-const fileHandler = function(req) {
-  if (req.url === '/') {
-    req.url = '/index.html';
-  }
-
-  if (req.method === 'GET' && req.url === '/guestBook.html') {
-    return serveGuestBook;
-  }
-
-  req.url = `${STATIC_FOLDER}${req.url}`;
-
-  if (req.method === 'POST') {
-    return servePost;
-  }
-
-  if (req.method === 'GET') {
-    return serveStaticFiles;
-  }
-
-  return () => new Response();
-};
 
 const respondOnConnect = function(socket) {
   const remote = `${socket.remoteAddress}: ${socket.remotePort}`;
@@ -79,8 +15,7 @@ const respondOnConnect = function(socket) {
   socket.on('data', text => {
     console.warn(`${remote} data:\n`);
     const req = Request.parse(text);
-    const handler = fileHandler(req);
-    const res = handler(req);
+    const res = processRequest(req);
     res.writeTo(socket);
   });
 };
