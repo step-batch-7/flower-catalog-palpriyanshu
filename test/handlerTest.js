a6da24eb92dce80ffe253a410d9b36a9c080835d
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-01-31 10:30:07 +0530

    extract responses into another file

--- test/handlerTest.js
+++ test/handlerTest.js
@@ -1,12 +1,15 @@
 const request = require('supertest');
 const {app} = require('../lib/handler.js');
 
+const STATUS_CODE2X = 200;
+
 describe('GET /', function() {
-  it('respond with html', function(done) {
+  it('should respond with html', function(done) {
     request(app.serveRequest.bind(app))
       .get('/')
       .set('Accept', '*/*')
       .expect('Content-Type', /html/)
-      .expect(200, done);
+      .expect(STATUS_CODE2X, done);
   });
 });
+
